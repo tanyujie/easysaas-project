63e89ce05fa6540359b4f434ebc4c70a8074e280
Administrator <Administrator@SC-201907201119>
2020-01-05 16:26:05 +0800

    init

--- easysaas-gateway/src/main/java/org/easymis/easysaas/gateway/GatewayApplication.java
+++ easysaas-gateway/src/main/java/org/easymis/easysaas/gateway/GatewayApplication.java
@@ -1,35 +1,47 @@
 package org.easymis.easysaas.gateway;
-
-
-
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-@ComponentScan(basePackages = "org.easymis.easycompany")
-@EnableTransactionManagement(order = 2) // 设置事务执行顺序(需要在切换数据源之后，否则只走主库)
-@MapperScan(basePackages = "org.easymis.easycompany.entitys.mybatis.mapper")
-@ServletComponentScan
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds =  60*60*8 )
-@EnableSwagger2
+@SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
-	protected static final Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
-
 	public static void main(String[] args) {
-		logger.info("search开始加载");
 		SpringApplication.run(GatewayApplication.class, args);
-		logger.info("search加载完毕");
+	}
+	@RequestMapping("/hystrixfallback")
+	public String hystrixfallback() {
+		return "This is a fallback";
+	}
+
+	@Bean
+	RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 2);
+	}
+
+	@Bean
+	SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+		return http.httpBasic().and()
+				.csrf().disable()
+				.authorizeExchange()
+				.pathMatchers("/anything/**").authenticated()
+				.anyExchange().permitAll()
+				.and()
+				.build();
+	}
+
+	@Bean
+	public MapReactiveUserDetailsService reactiveUserDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+		return new MapReactiveUserDetailsService(user);
 	}
 
 }
