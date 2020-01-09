9a50ce168a1f427c7fa36c9c19e4e5140d69304b
tanyujie <13551259347@139.com>
2020-01-09 17:29:45 +0800

    init

---
+++ easysaas-gateway/src/main/java/org/easymis/easysaas/gateway/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package org.easymis.easysaas.gateway.config;
+
+/*@EnableWebFluxSecurity
+public class WebSecurityConfig {
+	  @Bean
+	    SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+	        http.authorizeExchange().pathMatchers("/monitor/**").hasRole("ADMIN")
+	                .anyExchange().permitAll().and().cors().and()
+	                .httpBasic().and()
+	                .csrf().disable();
+	        return http.build();
+	    }
+
+}*/
