e4f32518dfeed7e04ab529c771b9a69b124a812d
tanyujie <13551259347@139.com>
2020-01-09 15:32:53 +0800

    init

--- easysaas-gateway/src/main/java/org/easymis/easysaas/gateway/config/LocalDateTimeSerializerConfig.java
+++ easysaas-gateway/src/main/java/org/easymis/easysaas/gateway/config/LocalDateTimeSerializerConfig.java
@@ -1,14 +1,15 @@
 package org.easymis.easysaas.gateway.config;
 
 
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 /**
  * 全局日期格式化处理，同时还兼顾了 Date 和 LocalDateTime 并存
