98cee49c8d255e4f836e2fc5871eda70c9e0123f
tanyujie <13551259347@139.com>
2020-01-10 17:37:57 +0800

    initial

---
+++ easysaas-gateway-test-a/src/main/java/com/xinyue/server/XinyueServerA.java
@@ -0,0 +1,17 @@
+package com.xinyue.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class XinyueServerA {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(XinyueServerA.class);
+        app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.run(args);
+    }
+}
