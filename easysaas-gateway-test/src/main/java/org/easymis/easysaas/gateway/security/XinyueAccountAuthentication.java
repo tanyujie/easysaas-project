9fcb679afde550d2218b46a220700e5491eb1d5a
tanyujie <13551259347@139.com>
2020-01-10 12:03:46 +0800

    init

---
+++ easysaas-gateway-test/src/main/java/org/easymis/easysaas/gateway/security/XinyueAccountAuthentication.java
@@ -0,0 +1,31 @@
+package org.easymis.easysaas.gateway.security;
+
+import java.util.Collection;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+public class XinyueAccountAuthentication extends AbstractAuthenticationToken{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private Object credentials;
+    private String principal;
+    
+    public XinyueAccountAuthentication(Object credentials,String principal,Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.credentials = credentials;
+        this.principal = principal;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    @Override
+    public String getPrincipal() {
+        return this.principal;
+    }
+
+}
