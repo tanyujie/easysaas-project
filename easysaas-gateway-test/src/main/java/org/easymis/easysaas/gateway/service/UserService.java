f59529a831baf7f438dabaa2b42cfab1deb6c378
Administrator <Administrator@SC-201907201119>
2020-01-11 21:23:34 +0800

    init

--- easysaas-gateway-test/src/main/java/org/easymis/easysaas/gateway/service/UserService.java
+++ easysaas-gateway-test/src/main/java/org/easymis/easysaas/gateway/service/UserService.java
@@ -1,38 +1,12 @@
 package org.easymis.easysaas.gateway.service;
 
-import java.util.Arrays;
-
-import org.easymis.easysaas.gateway.model.Role;
 import org.easymis.easysaas.gateway.model.User;
-import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Mono;
 
-/**
- *
- * @author ard333
- */
-@Service
-public class UserService {
-	
-	// this is just an example, you can load the user from the database from the repository
-	
-	//username:passwowrd -> user:user
-	private final String userUsername = "user";// password: user
-	private final User user = new User(userUsername, "cBrlgyL2GI2GINuLUUwgojITuIufFycpLG4490dhGtY=", true, Arrays.asList(Role.ROLE_USER));
-	
-	//username:passwowrd -> admin:admin
-	private final String adminUsername = "admin";// password: admin
-	private final User admin = new User(adminUsername, "dQNjUIMorJb8Ubj2+wVGYp6eAeYkdekqAcnYp+aRq5w=", true, Arrays.asList(Role.ROLE_ADMIN));
-	
-	public Mono<User> findByUsername(String username) {
-		if (username.equals(userUsername)) {
-			return Mono.just(user);
-		} else if (username.equals(adminUsername)) {
-			return Mono.just(admin);
-		} else {
-			return Mono.empty();
-		}
-	}
+
+public interface UserService {
+	public Mono<User> findByUsername(String username);	
+
 	
 }
---
+++ easysaas-gateway-test/src/main/java/org/easymis/easysaas/gateway/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package org.easymis.easysaas.gateway.service.impl;
+
+import java.util.Arrays;
+
+import org.easymis.easysaas.gateway.model.Role;
+import org.easymis.easysaas.gateway.model.User;
+import org.easymis.easysaas.gateway.service.UserService;
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Mono;
+@Service
+public class UserServiceImpl implements UserService {
+
+	//username:passwowrd -> user:user
+	private final String userUsername = "user";// password: user
+	private final User user = new User(userUsername, "cBrlgyL2GI2GINuLUUwgojITuIufFycpLG4490dhGtY=", true, Arrays.asList(Role.ROLE_USER));
+	
+	//username:passwowrd -> admin:admin
+	private final String adminUsername = "admin";// password: admin
+	private final User admin = new User(adminUsername, "dQNjUIMorJb8Ubj2+wVGYp6eAeYkdekqAcnYp+aRq5w=", true, Arrays.asList(Role.ROLE_ADMIN));
+	
+	public Mono<User> findByUsername(String username) {
+		if (username.equals(userUsername)) {
+			return Mono.just(user);
+		} else if (username.equals(adminUsername)) {
+			return Mono.just(admin);
+		} else {
+			return Mono.empty();
+		}
+	}
+}
