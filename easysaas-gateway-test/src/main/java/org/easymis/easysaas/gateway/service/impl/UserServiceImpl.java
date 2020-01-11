9b6d4e8a6f966a2bc11b331817e59f745e58d6ea
Administrator <Administrator@SC-201907201119>
2020-01-12 00:01:01 +0800

    initial

--- easysaas-gateway-test/src/main/java/org/easymis/easysaas/gateway/service/impl/UserServiceImpl.java
+++ easysaas-gateway-test/src/main/java/org/easymis/easysaas/gateway/service/impl/UserServiceImpl.java
@@ -2,15 +2,21 @@ package org.easymis.easysaas.gateway.service.impl;
 
 import java.util.Arrays;
 
+import org.easymis.easysaas.gateway.config.datasource.DataSourceType;
+import org.easymis.easysaas.gateway.config.datasource.EasymisDataSource;
+import org.easymis.easysaas.gateway.entitys.mybatis.mapper.UserMapper;
 import org.easymis.easysaas.gateway.entitys.vo.Role;
 import org.easymis.easysaas.gateway.entitys.vo.User;
 import org.easymis.easysaas.gateway.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Mono;
 @Service
 public class UserServiceImpl implements UserService {
-
+	
+	@Autowired
+	UserMapper mapper;
 	//username:passwowrd -> user:user
 	private final String userUsername = "user";// password: user
 	private final User user = new User(userUsername, "cBrlgyL2GI2GINuLUUwgojITuIufFycpLG4490dhGtY=", true, Arrays.asList(Role.ROLE_USER));
@@ -29,11 +35,8 @@ public class UserServiceImpl implements UserService {
 		}
 	}
 
-	@Override
+	@EasymisDataSource(DataSourceType.Master)
 	public User findByMobile(String mobile) {
-		if (mobile.equals(userUsername)) {
-			return user;
-		}
-		return admin;
+		return mapper.findByPhoneNumber(mobile);
 	}
 }
