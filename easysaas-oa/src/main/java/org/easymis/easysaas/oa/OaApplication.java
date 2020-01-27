b7b898293a975fd22a65a6fcd1569c65020803de
Administrator <Administrator@SC-201907201119>
2020-01-27 17:12:18 +0800

    init

---
+++ easysaas-oa/src/main/java/org/easymis/easysaas/oa/OaApplication.java
@@ -0,0 +1,24 @@
+package org.easymis.easysaas.oa;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableTransactionManagement(order = 2)
+@EnableDiscoveryClient
+@MapperScan({"*.easymis.easysaas.**.mapper"})
+@ComponentScan({"*.easymis.easysaas.*"})
+@EnableCaching
+public class OaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaApplication.class, args);
+	}
+}
