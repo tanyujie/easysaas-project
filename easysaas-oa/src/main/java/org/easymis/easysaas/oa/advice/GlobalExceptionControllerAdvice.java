a74e5a8d94d8f42733711811294910d178daee4b
Administrator <Administrator@SC-201907201119>
2020-01-28 10:07:29 +0800

    init

---
+++ easysaas-oa/src/main/java/org/easymis/easysaas/oa/advice/GlobalExceptionControllerAdvice.java
@@ -0,0 +1,8 @@
+package org.easymis.easysaas.oa.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionControllerAdvice {
+
+}
---
+++ easysaas-oa/src/main/java/org/easymis/easysaas/oa/advice/GolbalControllerRequestAndResponseAdvice.java
@@ -0,0 +1,10 @@
+package org.easymis.easysaas.oa.advice;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component//定义一个切面
+public class GolbalControllerRequestAndResponseAdvice {
+
+}
