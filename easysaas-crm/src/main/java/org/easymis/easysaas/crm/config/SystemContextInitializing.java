2eb4356c005cba09f14fea32944e54661ab22e68
Administrator <Administrator@SC-201907201119>
2020-02-20 20:30:29 +0800

    initial

---
+++ easysaas-crm/src/main/java/org/easymis/easysaas/crm/config/SystemContextInitializing.java
@@ -0,0 +1,55 @@
+package org.easymis.easysaas.crm.config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+@Service
+public class SystemContextInitializing implements InitializingBean {
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.setProperty("provinceJson",this.provinceJson().toString());
+		System.setProperty("categoryThreeLevelJson",this.categoryThreeLevelJson().toString());
+	}
+	private Object provinceJson() {
+		Object obj = new Object();
+		try {
+			obj = readFile("paramDictJson/districtJson.json");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+	private Object categoryThreeLevelJson() {
+		Object obj = new Object();
+		try {
+			obj = readFile("paramDictJson/CategoryThreeLevel.json");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+	private  Object readFile(String filePath) throws IOException {
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filePath);
+		String totalLine = "";
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				totalLine += line;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Object parse = JSONObject.parse(totalLine);
+		return parse;
+	}
+}
