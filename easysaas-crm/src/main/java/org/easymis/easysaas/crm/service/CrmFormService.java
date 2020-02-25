71cb5cbd44a792f680a8950bad12e1e49e46cf36
Administrator <Administrator@SC-201907201119>
2020-02-25 22:31:13 +0800

    init

--- easysaas-crm/src/main/java/org/easymis/easysaas/crm/service/CrmFormService.java
+++ easysaas-crm/src/main/java/org/easymis/easysaas/crm/service/CrmFormService.java
@@ -1,5 +1,18 @@
 package org.easymis.easysaas.crm.service;
 
-public interface CrmFormService {
+import org.easymis.easysaas.common.result.RestResult;
+import org.easymis.easysaas.crm.entitys.mybatis.dto.CrmForm;
 
+import com.github.pagehelper.PageInfo;
+
+public interface CrmFormService {
+	public boolean save(CrmForm bean);
+
+	public boolean update(CrmForm bean);
+
+	public CrmForm findById(String id);
+
+	public PageInfo findByOrgId(String orgId, Integer pageNum, Integer pageSize);
+
+	public RestResult deleteByIds(String ids);
 }
--- easysaas-crm/src/main/java/org/easymis/easysaas/crm/service/impl/CrmFormServiceImpl.java
+++ easysaas-crm/src/main/java/org/easymis/easysaas/crm/service/impl/CrmFormServiceImpl.java
@@ -1,9 +1,45 @@
 package org.easymis.easysaas.crm.service.impl;
 
+import org.easymis.easysaas.common.result.RestResult;
+import org.easymis.easysaas.crm.entitys.mybatis.dto.CrmForm;
 import org.easymis.easysaas.crm.service.CrmFormService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageInfo;
 
 @Service
 public class CrmFormServiceImpl implements CrmFormService{
+	@Autowired
+	private CrmFormMapper mapper;
+	@Override
+	public boolean save(CrmForm bean) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(CrmForm bean) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public CrmForm findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PageInfo findByOrgId(String orgId, Integer pageNum, Integer pageSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RestResult deleteByIds(String ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
