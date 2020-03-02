17a96687e276aab0330f7481f0ef73e56efe4363
tanyujie <13551259347@139.com>
2020-03-02 14:28:47 +0800

    init

--- easysaas-crm/src/main/java/org/easymis/easysaas/crm/entitys/mybatis/dto/CrmCustomerAllot.java
+++ easysaas-crm/src/main/java/org/easymis/easysaas/crm/entitys/mybatis/dto/CrmCustomerAllot.java
@@ -1,5 +1,31 @@
 package org.easymis.easysaas.crm.entitys.mybatis.dto;
 
-public class CrmCustomerAllot {
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class CrmCustomerAllot implements Serializable {
+	//@GenField(labelname = "", column = "allot_id", id = true, length = 40, isnull = false)
+	private String allotId;
+	//@GenField(labelname = "分配主式，auto/get/manual", column = "allot_type", length = 16, isnull = true)
+	private String allotType;
+	//@GenField(labelname = "分配人", column = "create_id", length = 64, isnull = true)
+	private String createId;
+	//@GenField(labelname = "分配时间", column = "create_time", isnull = true)
+	private Date createTime;
+	//@GenField(labelname = "客户id", column = "customer_id", length = 64, isnull = true)
+	private String customerId;
+	//@GenField(labelname = "删除标识", column = "delete_flag", isnull = false)
+	private Boolean deleteFlag;
+	//@GenField(labelname = "持有人", column = "hold_id", length = 64, isnull = true)
+	private String holdId;
+	//@GenField(labelname = "0待跟进  1跟进中", column = "status", isnull = false)
+	private Boolean status;
+	//@GenField(labelname = "更新人", column = "update_id", length = 64, isnull = true)
+	private String updateId;
+	//@GenField(labelname = "更新时间", column = "update_time", isnull = true)
+	private Date updateTime;
 
 }
--- easysaas-crm/src/main/java/org/easymis/easysaas/crm/entitys/mybatis/mapper/CrmCustomerMapper.java
+++ easysaas-crm/src/main/java/org/easymis/easysaas/crm/entitys/mybatis/mapper/CrmCustomerMapper.java
@@ -1,5 +1,39 @@
 package org.easymis.easysaas.crm.entitys.mybatis.mapper;
 
-public interface CrmCustomerMapper {
+import java.util.HashMap;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.easymis.easysaas.crm.entitys.mybatis.dto.CrmCustomer;
+
+public interface CrmCustomerMapper {
+	@Select("select * from crm_customer")
+	public List<CrmCustomer> getList(HashMap<String, Object> params);
+
+	@Insert("insert into crm_customer(address,annual_revenue,bank_name,bank_no,category_id,city_id,code,country_id,create_time,creator_id,customer_id,customer_name,delete_id,delete_time,depict,district_id,email,fax,industry_id,kind_id,level1_industry_id,level2_industry_id,level3_industry_id,locked,org_id,origin_id,owner_id,ownership,phone,province_id,qq,rating,register_address,status,street,tax_no,update_id,update_time,url,zip_code)values(#{address},#{annualRevenue},#{bankName},#{bankNo},#{categoryId},#{cityId},#{code},#{countryId},#{createTime},#{creatorId},#{customerId},#{customerName},#{deleteId},#{deleteTime},#{depict},#{districtId},#{email},#{fax},#{industryId},#{kindId},#{level1IndustryId},#{level2IndustryId},#{level3IndustryId},#{locked},#{orgId},#{originId},#{ownerId},#{ownership},#{phone},#{provinceId},#{qq},#{rating},#{registerAddress},#{status},#{street},#{taxNo},#{updateId},#{updateTime},#{url},#{zipCode})")
+	public void save(CrmCustomer bean);
+
+	@Insert("insert into crm_customer(address,annual_revenue,bank_name,bank_no,category_id,city_id,code,country_id,create_time,creator_id,customer_id,customer_name,delete_id,delete_time,depict,district_id,email,fax,industry_id,kind_id,level1_industry_id,level2_industry_id,level3_industry_id,locked,org_id,origin_id,owner_id,ownership,phone,province_id,qq,rating,register_address,status,street,tax_no,update_id,update_time,url,zip_code)values(#{address},#{annualRevenue},#{bankName},#{bankNo},#{categoryId},#{cityId},#{code},#{countryId},#{createTime},#{creatorId},#{customerId},#{customerName},#{deleteId},#{deleteTime},#{depict},#{districtId},#{email},#{fax},#{industryId},#{kindId},#{level1IndustryId},#{level2IndustryId},#{level3IndustryId},#{locked},#{orgId},#{originId},#{ownerId},#{ownership},#{phone},#{provinceId},#{qq},#{rating},#{registerAddress},#{status},#{street},#{taxNo},#{updateId},#{updateTime},#{url},#{zipCode})")
+	public void saveBatch(List<CrmCustomer> beans);
+
+	@Update("UPDATE crm_customer SET address= #{address},annual_revenue= #{annualRevenue},bank_name= #{bankName},bank_no= #{bankNo},category_id= #{categoryId},city_id= #{cityId},code= #{code},country_id= #{countryId},create_time= #{createTime},creator_id= #{creatorId},customer_id= #{customerId},customer_name= #{customerName},delete_id= #{deleteId},delete_time= #{deleteTime},depict= #{depict},district_id= #{districtId},email= #{email},fax= #{fax},industry_id= #{industryId},kind_id= #{kindId},level1_industry_id= #{level1IndustryId},level2_industry_id= #{level2IndustryId},level3_industry_id= #{level3IndustryId},locked= #{locked},org_id= #{orgId},origin_id= #{originId},owner_id= #{ownerId},ownership= #{ownership},phone= #{phone},province_id= #{provinceId},qq= #{qq},rating= #{rating},register_address= #{registerAddress},status= #{status},street= #{street},tax_no= #{taxNo},update_id= #{updateId},update_time= #{updateTime},url= #{url},zip_code= #{zipCode} WHERE customer_id= #{customerId}")
+	public void update(CrmCustomer bean);
+
+	@Delete(" DELETE FROM crm_customer WHERE customer_id = #{customerId}")
+	public void delete(String customer_id);
+
+	public void removeBatch(List<String> list);
+
+	public void restoreBatch(List<String> list);
+
+	public void deleteBatch(List<String> list);
+
+	@Select("select * from crm_customer t WHERE t.customer_id = #{customerId}")
+	public CrmCustomer findById(String customer_id);
+
+	@Select(" SELECT t.* FROM crm_customer t }")
+	public List<CrmCustomer> findByIds(List<String> list);
 }
