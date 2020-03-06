032368a4c17d561de95a9a08a557c5093d727161
tanyujie <13551259347@139.com>
2020-03-06 12:11:57 +0800

    init

---
+++ easysaas-rocketmq/src/main/java/org/easymis/easysaas/rocketmq/RedisConfiguration.java
@@ -0,0 +1,24 @@
+package org.easymis.easysaas.rocketmq;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+public class RedisConfiguration {
+  @Autowired
+  private LettuceConnectionFactory lettuceConnectionFactory; 
+  
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+      RedisTemplate<String, Object> template = new RedisTemplate<>();
+      template.setKeySerializer(new StringRedisSerializer());
+      template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+      template.setConnectionFactory(lettuceConnectionFactory);
+      return template;
+  }
+}
