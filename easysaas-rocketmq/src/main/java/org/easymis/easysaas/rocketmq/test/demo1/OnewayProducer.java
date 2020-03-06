7c1200624c394297eab0da519ffe1971eb60f97b
tanyujie <13551259347@139.com>
2020-03-06 17:22:37 +0800

    init

---
+++ easysaas-rocketmq/src/main/java/org/easymis/easysaas/rocketmq/test/demo1/OnewayProducer.java
@@ -0,0 +1,33 @@
+package org.easymis.easysaas.rocketmq.test.demo1;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * 
+　(3)以单向模式发送消息(单向传输用于需要中等可靠性的情况，例如日志收集。)
+ */
+public class OnewayProducer {
+    public static void main(String[] args) throws Exception{
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("localhost:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                "TagA" /* Tag */,
+                ("Hello RocketMQ " +
+                    i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            producer.sendOneway(msg);
+
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
