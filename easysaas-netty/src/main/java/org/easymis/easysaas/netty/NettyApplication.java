ff82a31cf243c452960bd9971a3b461def31bd23
Administrator <Administrator@SC-201907201119>
2020-03-07 15:58:29 +0800

    init

--- easysaas-netty/src/main/java/org/easymis/easysaas/netty/NettyApplication.java
+++ easysaas-netty/src/main/java/org/easymis/easysaas/netty/NettyApplication.java
@@ -3,12 +3,12 @@ package org.easymis.easysaas.netty;
 
 
 
-import java.util.logging.Logger;
-
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class NettyApplication {
 	protected static final Logger logger = LoggerFactory.getLogger(NettyApplication.class);
 
--- easysaas-netty/src/main/java/org/easymis/easysaas/netty/test/demo1/webscoket/WSScoket.java
+++ easysaas-netty/src/main/java/org/easymis/easysaas/netty/test/demo1/webscoket/WSScoket.java
@@ -1,5 +1,30 @@
 package org.easymis.easysaas.netty.test.demo1.webscoket;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class WSScoket {
 
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(mainGroup, subGroup)
+                            .channel(NioServerSocketChannel.class)
+                            .childHandler(new WSServerInitialzer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8088).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+
+
+    }
 }
