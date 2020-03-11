a55b0f85d352d63ec23ce8e406d5b73ece5bf87b
Administrator <Administrator@SC-201907201119>
2020-03-11 22:21:28 +0800

    initial

--- easysaas-springmvc/src/main/java/org/easymis/easysaas/mvc/rocketmq/service/ProducerService.java
+++ easysaas-springmvc/src/main/java/org/easymis/easysaas/mvc/rocketmq/service/ProducerService.java
@@ -1,8 +1,10 @@
 package org.easymis.easysaas.mvc.rocketmq.service;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.easymis.easysaas.mvc.rocketmq.domain.OrderPaidEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,6 +30,25 @@ public class ProducerService {
 		try {
 			msg.setBody("OK".getBytes());
 			SendResult sr=defaultMQProduce.send(msg);
+			
+			
+			 Message msg2 = new Message("Jodie_topic_1023",
+	                    "TagA",
+	                    "OrderID188",
+	                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+			 //发送2
+			defaultMQProduce.send(msg2, new SendCallback() {
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    System.out.printf("%-10d OK %s %n", 1, sendResult.getMsgId());
+                }
+
+                @Override
+                public void onException(Throwable e) {
+                    System.out.printf("%-10d Exception %s %n", 1, e);
+                    e.printStackTrace();
+                }
+			});
 			System.out.print(sr);
 		}catch(Exception e) {
 			System.out.println(e.toString());
