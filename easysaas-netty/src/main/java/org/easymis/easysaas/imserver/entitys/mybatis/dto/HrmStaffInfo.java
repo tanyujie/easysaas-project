f843365736e592b6051fc1c895fd21bf35dcc73c
Administrator <Administrator@SC-201907201119>
2020-03-15 12:49:01 +0800

    init

---
+++ easysaas-netty/src/main/java/org/easymis/easysaas/imserver/entitys/mybatis/dto/HrmStaffInfo.java
@@ -0,0 +1,11 @@
+package org.easymis.easysaas.imserver.entitys.mybatis.dto;
+
+import lombok.Data;
+
+@Data
+public class HrmStaffInfo {
+	private String staffId;
+	private String name;
+	private String orgId;
+	private String departmentId;
+}
---
+++ easysaas-netty/src/main/java/org/easymis/easysaas/imserver/entitys/vo/StaffInfoVo.java
@@ -0,0 +1,12 @@
+package org.easymis.easysaas.imserver.entitys.vo;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class StaffInfoVo {
+	private String staffId;
+	private String name;
+	private String orgId;
+}
