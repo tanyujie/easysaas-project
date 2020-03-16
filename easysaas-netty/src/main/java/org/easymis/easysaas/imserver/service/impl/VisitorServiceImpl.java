6b30991d8c3ebe45b305776d32fc654566538ba6
tanyujie <13551259347@139.com>
2020-03-16 14:46:01 +0800

    init

---
+++ easysaas-netty/src/main/java/org/easymis/easysaas/imserver/service/impl/VisitorServiceImpl.java
@@ -0,0 +1,8 @@
+package org.easymis.easysaas.imserver.service.impl;
+
+import org.easymis.easysaas.imserver.service.VisitorService;
+import org.springframework.stereotype.Service;
+@Service
+public class VisitorServiceImpl implements VisitorService {
+
+}
