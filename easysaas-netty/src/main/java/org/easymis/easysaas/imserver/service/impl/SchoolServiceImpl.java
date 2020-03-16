18140653ac3ed1b3630d1ac054ea8d57b3c9aab3
tanyujie <13551259347@139.com>
2020-03-16 15:41:42 +0800

    init

---
+++ easysaas-netty/src/main/java/org/easymis/easysaas/imserver/service/impl/SchoolServiceImpl.java
@@ -0,0 +1,16 @@
+package org.easymis.easysaas.imserver.service.impl;
+
+import java.util.List;
+
+import org.easymis.easysaas.imserver.service.SchoolService;
+import org.springframework.stereotype.Service;
+@Service
+public class SchoolServiceImpl implements SchoolService {
+
+	@Override
+	public List findList(String orgId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ easysaas-netty/src/main/java/org/easymis/easysaas/imserver/service/impl/SubjectServiceImpl.java
@@ -0,0 +1,16 @@
+package org.easymis.easysaas.imserver.service.impl;
+
+import java.util.List;
+
+import org.easymis.easysaas.imserver.service.SubjectService;
+import org.springframework.stereotype.Service;
+@Service
+public class SubjectServiceImpl implements SubjectService {
+
+	@Override
+	public List findList(String orgId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
