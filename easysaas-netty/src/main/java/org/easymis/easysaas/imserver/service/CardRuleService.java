7f008bd2747f0eccafbc6df39cd9186f729a8899
tanyujie <13551259347@139.com>
2020-03-16 15:23:49 +0800

    init

---
+++ easysaas-netty/src/main/java/org/easymis/easysaas/imserver/service/CardRuleService.java
@@ -0,0 +1,8 @@
+package org.easymis.easysaas.imserver.service;
+
+import org.easymis.easysaas.imserver.entitys.mybatis.dto.CardRule;
+
+public interface CardRuleService  {
+	CardRule findByOrgId(String orgId);
+	void saveOrUpdate(CardRule bean);
+}
---
+++ easysaas-netty/src/main/java/org/easymis/easysaas/imserver/service/SchoolService.java
@@ -0,0 +1,7 @@
+package org.easymis.easysaas.imserver.service;
+
+import java.util.List;
+
+public interface SchoolService {
+	List findList(String orgId);
+}
