1ef8d0c16df53e8e60676fe426e66169a51ee9df
Administrator <Administrator@10.0.0.1>
2020-03-19 21:48:14 +0800

    init

--- easyicc-project/easyicc-web/easyicc-card-admin/src/main/java/org/easymis/easyicc/card/admin/controller/setting/ThirdPartyConfigController.java
+++ easyicc-project/easyicc-web/easyicc-card-admin/src/main/java/org/easymis/easyicc/card/admin/controller/setting/ThirdPartyConfigController.java
@@ -1,5 +1,13 @@
 package org.easymis.easyicc.card.admin.controller.setting;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "/setting/thirdParty", description = "第三方接口配置")
+@Controller
+@RequestMapping("/thirdParty")
 public class ThirdPartyConfigController {
 
 }
